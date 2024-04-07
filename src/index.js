a876e4cb96321ef5a5ba73ed1e52939e68f85738
Kishh (Kishan) Salvi <88472719+Kishh14@users.noreply.github.com>
2024-04-07 18:47:36 +0000

    forgot to link the footer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Footer from './components/Footer'
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
@@ -10,6 +11,7 @@ root.render(
   <BrowserRouter>
     {/* <React.StrictMode> */}
     <App />
+    <Footer/>
     {/* </React.StrictMode> */}
   </BrowserRouter>
 );
